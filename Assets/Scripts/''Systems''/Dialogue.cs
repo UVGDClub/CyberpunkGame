166d2b01f6d7bdcbca3241b534c22870f4438157
aldman123 <alexander@aldridge.ca>
2018-10-05 14:00:16 -0700

    Worked on loading from text and parsing text

--- Assets/Scripts/''Systems''/Dialogue.cs
+++ Assets/Scripts/''Systems''/Dialogue.cs
@@ -9,11 +9,13 @@ public class Dialogue : MonoBehaviour {
 
     public Text dialogueText;
     float stopDisplayTime;
+    int MAX_SENTANCE_LENGTH = 50;
 
     // Use this for initialization
     void Start () {
         stopDisplayTime = 0;
         dialogueText.text = "Assume cat is flat";
+        this.LoadTextFile("Pulp Fiction");
 	}
 	
 	void FixedUpdate () {
@@ -31,12 +33,12 @@ public class Dialogue : MonoBehaviour {
 
     /*
      * @param dialogue -> the text to be displayed
-     * Will display the text for eight seconds
+     * Will display the text for sixteen seconds
      */
     public void SetDialogue(string dialogue)
     {
         dialogueText.text = dialogue;
-        stopDisplayTime = Time.fixedTime + 8;
+        stopDisplayTime = Time.fixedTime + 16;
     }
     /*
      * @param textTime -> the amount of time that the text should be on the screen
@@ -45,6 +47,37 @@ public class Dialogue : MonoBehaviour {
     {
         dialogueText.text = dialogue;
         stopDisplayTime = Time.fixedTime + textTime;
+    }
+
+    /**
+     * @param location -> the name of the text file within "Resources/Dialogue"
+     * Example: "Pulp Fiction" will load "Pulp Fiction.txt" from Resources/Dialogue
+     */
+    public void LoadTextFile(string name)
+    {
+        string text = Resources.Load<TextAsset>("Dialgue/Pulp Fiction").ToString();
+        string[] sentances = this.ParseText(text);
+
+        SetDialogue(text); //Change this once parsing is complete
+    }
+
+    private string[] ParseText(string text)
+    {
+        List<string> output = new List<string>();
+        string[] words = text.Split(' ');
+
+        int sentanceNumber = 0;
+        foreach (string word in words)
+        {
+            if (output[sentanceNumber].Length + word.Length > MAX_SENTANCE_LENGTH)
+            {
+                sentanceNumber++;
+                output.Add("");
+            }
+            output[sentanceNumber] += word;
+        }
+
+        return output.ToArray();
     }
 
 }
