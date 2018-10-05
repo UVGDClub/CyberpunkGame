bb5de3d23c75be88e49c7c37b6d0c48e1b231ad9
aldman123 <alexander@aldridge.ca>
2018-10-04 20:01:56 -0700

    Added basic ass dialogue (it's not great)

---
+++ Assets/Scripts/''Systems''/Dialogue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour {
+
+    public Text dialogueText;
+
+	// Use this for initialization
+	void Start () {
+        dialogueText.text = "Default";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+	}
+
+    public void SetDialogue(string dialogue)
+    {
+        dialogueText.text = dialogue;
+    }
+
+}
---
+++ Assets/Scripts/''Systems''/DialogueTesting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTesting : MonoBehaviour {
+
+    public Dialogue dialogue;
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+
+    [ContextMenu("Wizard Man")]
+    void SetWizardMan()
+    {
+        dialogue.SetDialogue("Alexander is the wizard");
+    }
+}
